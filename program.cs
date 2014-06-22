2dba41e5b4351f440376c696a059865f3dae2c91
tchang82 <tchang82@yahoo.com>
2014-06-22 11:16:56 -0700

    NO semicolon buildbreak
    Updating with something that will cause a build break

--- program.cs
+++ program.cs
@@ -4,4 +4,9 @@ public class HelloWorld
     public static void Main(string[] args) {
 	Console.Write("Hello World!");
     }
+    
+    private static string GetName()
+    {
+    	return "tchang82" //intentionally forgetting the ending semicolon
+    }
 }
